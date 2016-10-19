e624aece8d639755332905a01946f1f6f6acfbb7
Arturo Arévalo <arturo.arevalo.gonzalez@gmail.com>
2016-10-19 18:00:27 +0200

    Created Settings class

--- source/lib/main.ts
+++ source/lib/main.ts
@@ -1,5 +1,15 @@
 import * as restify from 'restify';
 import logger from './logger';
+import settings from './settings';
+
+// Initialize settings.
+try {
+    settings.initialize();
+} catch (ex) {
+    logger.error(ex);
+    process.exit(1);
+}
+
 
 const server = restify.createServer();
 server.get('/', (req, res) => {
